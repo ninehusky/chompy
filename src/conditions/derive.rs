ed721bc2921c607363035c5b85f2c2a379597119
Andrew Cheung <a7cheung@ucsd.edu>
2025-04-29 15:48:21 -0700

    Checkpoint

--- src/conditions/derive.rs
+++ src/conditions/derive.rs
@@ -1,8 +1,31 @@
 // This file is about minimizing implications :)
 
+use std::str::FromStr;
+
 use egg::Rewrite;
 
-use crate::{halide::Pred, SynthAnalysis};
+use crate::{enumo, halide::Pred, SynthAnalysis, SynthLanguage, ValidationResult};
+
+use super::{validate_implication, Implication};
+
+// We assume a low score is better.
+// For now, this is just the AST size of lhs + rhs.
+// later, we can use 1 / |satisfying_lhs_envs| + |satisfying_rhs_envs| as a proxy
+// for weakness.
+fn score<L: SynthLanguage>(imp: &Implication<L>) -> usize {
+    fn size(sexp: &enumo::Sexp) -> usize {
+        match sexp {
+            enumo::Sexp::Atom(_) => 1,
+            enumo::Sexp::List(l) => l.iter().map(size).sum(),
+        }
+    }
+
+
+    let lhs = enumo::Sexp::from_str(&imp.lhs.to_string()).unwrap();
+    let rhs = enumo::Sexp::from_str(&imp.rhs.to_string()).unwrap();
+
+    size(&lhs) + size(&rhs)
+}
 
 // Until there are no more imps to consider:
 // 1. Pop the "best" implication from imps.
@@ -10,8 +33,93 @@ use crate::{halide::Pred, SynthAnalysis};
 //    In particular, add `(edge lhs rhs)` to the egraph.
 // 3. Run the path-finding algorithm on the egraph.
 // 4. For each remaining implication, query the egraph to see if `(path lhs rhs)` is present. If it is, throw the rule away.
-pub fn minimize_implications(imps: Vec<Rewrite<Pred, SynthAnalysis>>, prior: Vec<Rewrite<Pred, SynthAnalysis>>) -> Vec<Rewrite<Pred, SynthAnalysis>> {
-    vec![]
+// TODO: get rid of `Pred`, and boot the need to validate over to the `SynthLanguage` trait.
+pub fn minimize_implications(imps: &mut Vec<Implication<Pred>>, prior: &Vec<Implication<Pred>>) -> (Vec<Implication<Pred>>, Vec<Implication<Pred>>) {
+    let mut invalid: Vec<Implication<Pred>> = vec![];
+    let mut chosen = prior.clone();
+    let step_size = 1;
+    while !imps.is_empty() {
+        let selected = select_implications(imps, step_size, &mut invalid);
+        println!("done selecting");
+        // assert_eq!(selected.len(), 1); <-- wasn't this here in ruler?
+        chosen.extend(selected.clone());
+
+        shrink_implications(imps, &mut chosen);
+
+    }
+
+    let mut new_imps = vec![];
+    // Return only the new rules
+    for imp in imps {
+        if !chosen.contains(imp) {
+            new_imps.push(imp.clone());
+        }
+    }
+
+    (new_imps, invalid)
+
+}
+
+
+pub fn shrink_implications(imps: &mut Vec<Implication<Pred>>, chosen: &Vec<Implication<Pred>>) -> Vec<Implication<Pred>> {
+    // 1. make new egraph
+    let mut egraph = new_impl_egraph();
+
+    // 2. add the lhs, rhs of each implication to the egraph
+        // now, attempt to derive the selected implications.
+        // step 2
+        for imp in chosen {
+            let lhs = imp.lhs.to_string();
+            let rhs = imp.rhs.to_string();
+
+            egraph.parse_and_run_program(None,
+                format!("(edge {} {})", lhs, rhs).as_str()).unwrap();
+        }
+
+        // step 3
+        egraph.parse_and_run_program(None, "(run-schedule (saturate find-path))").unwrap();
+
+        let mut keep = vec![];
+
+        // step 4
+        for imp in imps {
+            let lhs = imp.lhs.to_string();
+            let rhs = imp.rhs.to_string();
+
+            if !egraph.parse_and_run_program(None,
+                format!("(check (path {} {}))", lhs, rhs).as_str()).is_ok() {
+                // the path does not exist
+                keep.push(imp.clone());
+            }
+
+        }
+
+    keep
+}
+
+pub fn select_implications(imps: &mut Vec<Implication<Pred>>, step_size: usize, invalid: &mut Vec<Implication<Pred>>) -> Vec<Implication<Pred>> {
+    let mut chosen = vec![];
+    imps.sort_by(| a, b| score(a).cmp(&score(b)));
+
+    let mut selected = vec![];
+
+    while selected.len() < step_size {
+        let popped = imps.pop();
+        if let Some(imp) = popped {
+            if matches!(validate_implication(imp.clone()), ValidationResult::Valid) {
+                selected.push(imp.clone());
+            } else {
+                invalid.push(imp.clone());
+            }
+        } else {
+            // we've run out of implications
+            break;
+        }
+    }
+
+    chosen.extend(selected);
+    chosen
+
 }
 
 /// Returns an egraph initialized with the path-finding ruleset.
--- src/conditions/mod.rs
+++ src/conditions/mod.rs
@@ -1,2 +1,78 @@
+use std::sync::Arc;
+use std::str::FromStr;
+
+use z3::ast::Ast;
+use egg::Pattern;
+
+use crate::{enumo, SynthLanguage, ValidationResult};
+use crate::halide::{Pred, egg_to_z3};
+
 pub mod generate;
-pub mod derive;
+mod derive;
+
+#[derive(Clone, PartialEq)]
+pub struct Implication<L: SynthLanguage> {
+    name: Arc<str>,
+    lhs: Pattern<L>,
+    rhs: Pattern<L>,
+}
+
+pub fn validate_implication(imp: Implication<Pred>) -> ValidationResult {
+    let mut cfg = z3::Config::new();
+    cfg.set_timeout_msec(1000);
+    let ctx = z3::Context::new(&cfg);
+    let solver = z3::Solver::new(&ctx);
+    let lexpr = egg_to_z3(&ctx, Pred::instantiate(&imp.lhs).as_ref());
+    let rexpr = egg_to_z3(&ctx, Pred::instantiate(&imp.rhs).as_ref());
+    // assert that lexpr --> rexpr.
+
+
+    // 1. we want to get rid of trivial implications
+    // because p -> q == !p or q, we can just check if
+    // - the LHS is not equal to 0, and
+    // - the RHS is not equal to 1.
+    
+    let zero = z3::ast::Int::from_i64(&ctx, 0);
+    let one = z3::ast::Int::from_i64(&ctx, 1);
+
+    // ask the solver to find a model where the LHS is true.
+    solver.assert(&lexpr._eq(&zero).not());
+
+    // if it can't, then the LHS is trivially false
+    if matches!(solver.check(), z3::SatResult::Unsat) {
+        // it's "invalid" in the sense that we want to ditch this implication because it's
+        // trivially true.
+        println!("{} is trivially false", imp.lhs.to_string());
+        return ValidationResult::Invalid;
+    }
+
+    solver.reset();
+
+    // ask the solver to find a model where the RHS is false.
+    solver.assert(&one._eq(&rexpr).not());
+
+    // if it can't, then the RHS is trivially true.
+    if matches!(solver.check(), z3::SatResult::Unsat) {
+        println!("{} is trivially true", imp.rhs.to_string());
+        return ValidationResult::Invalid;
+    }
+
+    solver.reset();
+
+    // with trivial implications out of the way, we can now check if the non-trivial implication is valid.
+
+    println!("checking implication: {} -> {}", lexpr, rexpr);
+    solver.assert(&z3::ast::Bool::implies(&lexpr._eq(&zero).not(), &rexpr._eq(&zero).not()).not());
+
+    let result = solver.check();
+    println!("result: {:?}", result);
+
+
+    match result {
+        z3::SatResult::Unsat => ValidationResult::Valid,
+        z3::SatResult::Unknown => ValidationResult::Unknown,
+        z3::SatResult::Sat => ValidationResult::Invalid,
+    }
+
+
+}
