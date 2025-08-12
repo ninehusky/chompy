b9873fd3f3625f467b1f078dcfe03cdb5ca7537e
Andrew Cheung <a7cheung@ucsd.edu>
2025-08-12 14:30:04 -0700

    Add a bunch of tests

--- src/halide.rs
+++ src/halide.rs
@@ -302,18 +302,16 @@ impl SynthLanguage for Pred {
               return Some(q);
             }),
             Pred::Mod([x, y]) => map!(get_cvec, x, y => {
-                let a = x;
-                let b = y;
-                let mut ia = a.clone();
-                let ib = b;
-                let a_neg = ia >> 63;
-                let b_neg = ib >> 63;
-                let b_zero: i64 = if *ib == 0 { -1 } else { 0 };
-                ia -= a_neg;
-                let mut r: i64 = ia % (ib | b_zero);
-                r += a_neg & ((ib ^ b_neg) + !b_neg);
+                let a: i64 = *x; // or x.clone() if x is i64, just make sure it's i64
+                let b: i64 = *y;
+                let a_neg = a >> 63;
+                let b_neg = b >> 63;
+                let b_zero = if b == 0 { -1 } else { 0 };
+                let ia = a - a_neg;
+                let mut r = ia % (b | b_zero);
+                r += a_neg & ((b ^ b_neg) + !b_neg);
                 r &= !b_zero;
-                return Some(r);
+                Some(r)
             }),
             Pred::Min([x, y]) => map!(get_cvec, x, y => Some(*x.min(y))),
             Pred::Max([x, y]) => map!(get_cvec, x, y => Some(*x.max(y))),
@@ -514,20 +512,22 @@ impl SynthLanguage for Pred {
         let lexpr = egg_to_z3(&ctx, Self::instantiate(lhs).as_ref());
         let rexpr = egg_to_z3(&ctx, Self::instantiate(rhs).as_ref());
         solver.assert(&lexpr._eq(&rexpr).not());
-        // if matches!(solver.check(), z3::SatResult::Sat) {
-        //     let model = solver.get_model().unwrap();
-        //     println!(
-        //         "Rule {} ==> {} is invalid.\nthe model is:\n{:?}eval({}) = {:?}\neval({}) = {:?}\n",
-        //         lhs,
-        //         rhs,
-        //         model,
-        //         lhs,
-        //         model.eval(&lexpr),
-        //         rhs,
-        //         model.eval(&rexpr)
-        //     );
-        // }
-        match solver.check() {
+        let res = solver.check();
+        println!("res: {:?}", res);
+        if matches!(res, z3::SatResult::Sat) {
+            let model = solver.get_model().unwrap();
+            println!(
+                "Rule {} ==> {} is invalid.\nthe model is:\n{:?}eval({}) = {:?}\neval({}) = {:?}\n",
+                lhs,
+                rhs,
+                model,
+                lhs,
+                model.eval(&lexpr, true),
+                rhs,
+                model.eval(&rexpr, true)
+            );
+        }
+        match res {
             z3::SatResult::Unsat => ValidationResult::Valid,
             z3::SatResult::Unknown => ValidationResult::Unknown,
             z3::SatResult::Sat => ValidationResult::Invalid,
@@ -1217,19 +1217,148 @@ pub fn og_recipe() -> Ruleset<Pred> {
     all_rules
 }
 
-// TODO
+#[cfg(test)]
 mod div_mod_tests {
-    use crate::enumo::Rule;
+    #[allow(unused_imports)]
+    use crate::{
+        enumo::{Rule, Ruleset, Workload},
+        EGraph, SynthAnalysis,
+    };
+    use crate::{SynthLanguage, ValidationResult};
 
     use super::Pred;
 
     #[test]
+    fn div_interpreter_ok() {
+        let pairs = vec![
+            ("(/ 1 2)", "0"),
+            ("(/ -4 -2)", "2"),
+            ("(/ 5 2)", "2"),
+            ("(/ -5 -2)", "3"),
+            // this is an instance where Halide division is
+            // different from C division.
+            ("(/ -5 2)", "-3"),
+            ("/ 1 0", "0"),
+        ];
+
+        for (l, r) in pairs {
+            let workload = Workload::new(&[l, r, "dummy_var"]);
+            let egraph: EGraph<Pred, SynthAnalysis> = workload.to_egraph();
+            let l_id = egraph.lookup_expr(&l.parse().unwrap()).unwrap();
+            let r_id = egraph.lookup_expr(&r.parse().unwrap()).unwrap();
+            assert_eq!(
+                egraph[l_id].data.cvec, egraph[r_id].data.cvec,
+                "Failed for {} == {}",
+                l, r
+            );
+        }
+    }
+
+    #[test]
+    fn div_validation_ok() {
+        let mut ruleset: Ruleset<Pred> = Default::default();
+        ruleset.add(Rule::from_string("(/ 1 2) ==> 0").unwrap().0);
+        ruleset.add(Rule::from_string("(/ -4 -2) ==> 2").unwrap().0);
+        ruleset.add(Rule::from_string("(/ 5 2) ==> 2").unwrap().0);
+        ruleset.add(Rule::from_string("(/ -5 -2) ==> 3").unwrap().0);
+
+        for (_, rule) in ruleset {
+            assert!(rule.is_valid(), "Rule is not valid: {}", rule);
+        }
+    }
+
+    #[test]
+    fn mod_interpreter_ok() {
+        let pairs = vec![
+            ("(% 1 2)", "1"),
+            ("(% -4 -2)", "0"),
+            ("(% 5 2)", "1"),
+            ("(% -5 -2)", "1"),
+            ("(% -5 2)", "1"),
+            ("(% 1 0)", "0"),
+        ];
+
+        for (l, r) in pairs {
+            let workload = Workload::new(&[l, r, "dummy_var"]);
+            let egraph: EGraph<Pred, SynthAnalysis> = workload.to_egraph();
+            let l_id = egraph.lookup_expr(&l.parse().unwrap()).unwrap();
+            let r_id = egraph.lookup_expr(&r.parse().unwrap()).unwrap();
+            assert_eq!(
+                egraph[l_id].data.cvec, egraph[r_id].data.cvec,
+                "Failed for {} == {}",
+                l, r
+            );
+        }
+    }
+
+    #[test]
+    fn mod_validation_ok() {
+        let mut ruleset: Ruleset<Pred> = Default::default();
+        ruleset.add(Rule::from_string("(% 1 2) ==> 1").unwrap().0);
+        ruleset.add(Rule::from_string("(% -4 -2) ==> 0").unwrap().0);
+        ruleset.add(Rule::from_string("(% 5 2) ==> 1").unwrap().0);
+        ruleset.add(Rule::from_string("(% -5 -2) ==> 1").unwrap().0);
+        ruleset.add(Rule::from_string("(% 1 0) ==> 0").unwrap().0);
+
+        for (_, rule) in ruleset {
+            assert!(rule.is_valid(), "Rule is not valid: {}", rule);
+        }
+    }
+
+    #[test]
+    fn mod_axioms_valid() {
+        let mut ruleset: Ruleset<Pred> = Default::default();
+        ruleset.add(Rule::from_string("(<= 0 (% ?a ?b)) ==> 1").unwrap().0);
+        ruleset.add(Rule::from_string("(< (% ?a ?b) (abs ?b)) ==> 1").unwrap().0);
+
+        for (_, rule) in ruleset {
+            assert!(rule.is_valid(), "Rule is not valid: {}", rule);
+        }
+    }
+
+    #[test]
+    fn euclidean_identity_interpreter() {
+        let workload = Workload::new(&["(+ (* (/ a b) b) (% a b))", "a"]);
+        let egraph: EGraph<Pred, SynthAnalysis> = workload.to_egraph();
+        let a_id = egraph.lookup_expr(&"a".parse().unwrap()).unwrap();
+        let b_id = egraph.lookup_expr(&"b".parse().unwrap()).unwrap();
+        let expr_id = egraph
+            .lookup_expr(&"(+ (* (/ a b) b) (% a b))".parse().unwrap())
+            .unwrap();
+        assert!(!egraph[a_id].data.cvec.is_empty());
+
+        for (idx, a_el) in egraph[a_id].data.cvec.iter().enumerate() {
+            let b_el = egraph[b_id].data.cvec.get(idx).unwrap();
+            if *b_el == Some(0) {
+                // Skip division by zero.
+                continue;
+            }
+            let expr_el = egraph[expr_id].data.cvec.get(idx).unwrap();
+            assert_eq!(
+                a_el, expr_el,
+                "Mismatch at index {}: a_el == {:?}, b_el == {:?}, expr_el == {:?}",
+                idx, a_el, b_el, expr_el
+            );
+        }
+    }
+
+    #[test]
+    // The euclidean identity should be valid. Our current implementation causes SMT
+    // to hang on this test, so I'm just ensuring that it's _not_ invalid for now.
     fn euclidean_identity_valid() {
         // if b != 0, then (a / b) * b + (a % b) == a
-        let rule: Rule<Pred> =
-            Rule::from_string("(+ (* (/ ?a ?b) ?b) (% ?a ?b)) ==> ?a if (!= ?b 0)")
-                .unwrap()
-                .0;
-        assert!(rule.is_valid());
+        // let rule: Rule<Pred> =
+        //     Rule::from_string("(+ (* (/ ?a ?b) ?b) (% ?a ?b)) ==> ?a if (!= ?b 0)")
+        //         .unwrap()
+        //         .0;
+        // assert!(rule.is_valid());
+        assert!(!matches!(
+            Pred::validate_with_cond(
+                &"(+ (* (/ ?a ?b) ?b) (% ?a ?b))".parse().unwrap(),
+                &"a".parse().unwrap(),
+                &"(!= ?b 0)".parse().unwrap()
+            ),
+            ValidationResult::Invalid
+        ));
     }
 }
