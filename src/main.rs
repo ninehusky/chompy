eb1d3d6892186616139313150a97dc84c50de0ba
Andrew Cheung <a7cheung@ucsd.edu>
2025-09-04 12:27:34 -0700

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use ruler::enumo::Ruleset;
+use ruler::halide::mini_recipe;
 use ruler::halide::Pred;
 
 use ruler::halide::og_recipe;
+use ruler::recipe_utils::LLMEnumerationConfig;
+use ruler::recipe_utils::LLMFilterConfig;
+use ruler::recipe_utils::LLMUsage;
 
 use std::fs::File;
 use std::path::PathBuf;
@@ -11,37 +15,18 @@ use clap::Parser;
 
 // Outlines how to perform Halide rule synthesis.
 #[derive(Debug)]
-pub enum ChompyMode {
-    HandwrittenRecipe,
-    LLMAlphabetSoup,
-    LLMRecipe,
+pub enum Recipe {
+    MiniRecipe,
+    FullRecipe,
 }
 
-impl FromStr for ChompyMode {
+impl FromStr for Recipe {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "handwritten" => Ok(Self::HandwrittenRecipe),
-            "llm_alphabet_soup" => Ok(Self::LLMAlphabetSoup),
-            "llm_recipes" => Ok(Self::LLMRecipe),
+            "mini" => Ok(Self::MiniRecipe),
+            "full" => Ok(Self::FullRecipe),
             _ => Err("Invalid mode.".to_string()),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum RecipeType {
-    // what we used to run: the default recipe with added nesting
-    // that we can't express in the JSON format.
-    OgRecipe,
-}
-
-impl FromStr for RecipeType {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "og_recipe" => Ok(Self::OgRecipe),
-            _ => Err("Invalid recipe type.".to_string()),
         }
     }
 }
@@ -49,46 +34,47 @@ impl FromStr for RecipeType {
 #[derive(Parser, Debug)]
 struct ChompyArgs {
     #[clap(long)]
-    mode: ChompyMode,
+    recipe: Recipe,
     #[clap(long)]
-    output_path: PathBuf,
+    llm_usage: String,
     #[clap(long)]
-    recipe_path: Option<PathBuf>,
-    #[clap(long)]
-    old_recipe_type: Option<String>,
+    output_path: String,
 }
 
 #[tokio::main]
 pub async fn main() {
     let args = ChompyArgs::parse();
-    let mode = args.mode;
-    let output_file = args.output_path;
+
+    let default_filter_cfg = LLMFilterConfig::default().with_on_threshold(10);
+    let default_enum_cfg = LLMEnumerationConfig::default().with_num_conditions(20).with_num_terms(100);
+
+    let llm_usage = match args.llm_usage.as_str() {
+        "baseline" => LLMUsage::None,
+        "enum_only" => LLMUsage::EnumerationOnly(default_enum_cfg.clone()),
+        "baseline_andenum" => LLMUsage::Enumeration(default_enum_cfg.clone()),
+        "baseline_and_filter_1" => LLMUsage::Filter(default_filter_cfg.clone().with_top_k(1)),
+        "baseline_and_filter_5" => LLMUsage::Filter(default_filter_cfg.clone().with_top_k(5)),
+        "baseline_with_filter_5_and_enum" => LLMUsage::Combined(
+            vec![
+                LLMUsage::Filter(default_filter_cfg.clone()),
+                LLMUsage::Enumeration(default_enum_cfg.clone()),
+            ]
+        ),
+        _ => panic!("Invalid llm_usage"),
+    };
 
     // create the output file if it doesn't exist.
-    if let Err(e) = File::create(output_file.clone()) {
+
+    if let Err(e) = File::create(&args.output_path) {
         panic!("Failed to create output file: {}", e);
     }
-    match mode {
-        // ChompyMode::HandwrittenRecipe => {
-        //     let old_recipe_type = args.old_recipe_type;
-        //     let recipe_path = args.recipe_path;
-        //     match (old_recipe_type, recipe_path) {
-        //         (Some(recipe_type), None) => {
-        //             let recipe: RecipeType = recipe_type.parse().unwrap();
-        //             match recipe {
-        //                 RecipeType::OgRecipe => og_recipe(),
-        //             }
-        //         }
-        //         (Some(_), Some(_)) => panic!("both recipe types provided."),
-        //         (None, _) => panic!("no recipe type provided."),
-        //     }
-        // }
-        // ChompyMode::LLMAlphabetSoup => run_gpt_eval().await,
-        // ChompyMode::LLMRecipe => {
-        _ => todo!("Not implemented yet.")
-    };
-}
 
-pub async fn run_gpt_eval() -> Ruleset<Pred> {
-    todo!("This is pretty out of date.");
+    let rules = match args.recipe {
+        Recipe::MiniRecipe => mini_recipe(llm_usage).await,
+        Recipe::FullRecipe => og_recipe(llm_usage).await,
+    };
+
+    rules.to_file(&args.output_path);
+
+    println!("Wrote {} rules to {:?}", rules.len(), args.output_path);
 }
